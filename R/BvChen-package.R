bb0b9ac92f84fd8755373e059461c7dd67a8b878
MukulBijalwan <mukulbijalwan555@gmail.com>
2026-08-25 16:55:20 +0530

    docs(cran): use real repo URLs in package help; drop placeholder maintainer block

--- R/BvChen-package.R
+++ R/BvChen-package.R
@@ -3,4 +3,10 @@
 # ---------------------------------------------------------------
 
 #' @keywords internal
+#' @details
+#' Help and issue reporting are available at the URLs below.
+#' \itemize{
+#'   \item \url{https://github.com/MukulBijalwan/BvChen}
+#'   \item Report bugs at \url{https://github.com/MukulBijalwan/BvChen/issues}
+#' }
 "_PACKAGE"
